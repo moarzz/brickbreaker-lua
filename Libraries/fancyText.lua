b01b7279b242a0457b4c9ac92dc6c9fb43b24262
Milo:3 Silli Denote <71358745+MiloX3-Silli-Denote@users.noreply.github.com>
2025-10-13 18:26:27 -0400

    Update fancyText.lua

--- Libraries/fancyText.lua
+++ Libraries/fancyText.lua
@@ -46,12 +46,12 @@ function FancyText.new(text, x, y, width, textHeight, alignment, font, dataRefer
     instance.x = x;
     instance.y = y;
 
-    instance.textHeight = (type(textHeight) == "number" and textHeight > 0) and textHeight or 20;
+    instance.textHeight = textHeight or 20;
 
     instance.width = width;
     instance.alignment = alignment or "left";
 
-    instance.text = text or "";
+    instance.text = text;
     instance.lines = nil; -- gets redefined in FancyText:alignText()
 
     instance.pointer = dataReference or {};
@@ -95,20 +95,14 @@ function FancyText:update()
 end
 
 function FancyText:alignText()
-    local realText = self.text or "";
+    local realText = self.text;
     realText = string.gsub(realText, "%b<>",
         function(strToReplace)
             -- check if it is changing a graphical component
             if string.find(strToReplace, "=") then
                 return nil; -- dont alter the string (yet)
             else -- otherwise its a key to the pointer table
-                local key = string.sub(strToReplace,2,-2)
-                local val = self.pointer[key]
-                if type(val) == "function" then
-                    return tostring(val())
-                else
-                    return tostring(val)
-                end
+                return tostring(self.pointer[string.sub(strToReplace,2,-2)]);
             end
         end
     );
@@ -140,6 +134,16 @@ function FancyText:alignText()
 
     local lineHeight = 0;
     local lineWidth = 0;
+
+    local imageScale = 1; -- scale x and y
+    local imageScaleX = 1; -- scale x
+    local imageScaleY = 1; -- scale y
+    local imageOffsetX = 0; -- move to the right by pixels
+    local imageOffsetY = 0; -- move down by pixels
+    local imageBuffer = 0; -- move following items right by pixels
+    local imageWidth = nil; -- force the image to be this wide
+    local imageHeight = nil; -- force the image to be this tall
+
     local scale = self.textHeight / self.fontHeight;
 
     self.lines = {}; -- empty the table
@@ -191,8 +195,45 @@ function FancyText:alignText()
 
                 parsedText = parsedText .. previousFontText;
                 tillNextSpace = string.sub(tillNextSpace, ind + 1,-1); -- remove the graphical change from the text
-            else -- if the graphical change is not a font change then dont alter anything
+            elseif string.find(modif, "^image=.*$") then -- if the modifier is to draw an image then handle it differently
+                local previousFontText = string.sub(tillNextSpace, 0, ind - 1); -- previously seen text
+
+                widthOfStr = widthOfStr + curFont:getWidth(previousFontText) * scale; -- add the width of the previous text to the width of the current line
+
+                local drawImage = self.pointer[(string.match(modif, "^image=(.*)$"))]; -- get the image we want to draw
+
+                assert(drawImage and drawImage:type() == "Image", "tried to draw a non image object");
+
+                -- move remaining text on this line to the left of the image
+                widthOfStr = widthOfStr + (imageWidth or (drawImage:getWidth() * imageScale * imageScaleX));
+                widthOfStr = widthOfStr + imageOffsetX + imageBuffer; -- offsets (make it visually seperate from the above line)
+
+                parsedText = parsedText .. previousFontText;
+                tillNextSpace = string.sub(tillNextSpace, ind + 1,-1); -- remove the graphical change from the text
+
+                -- have the line heihgt change depending on the height of the image (notice offsets are different from scales, as it is in the width)
+                lineHeight = math.max(lineHeight, imageHeight or (drawImage:getHeight() * imageScale * imageScaleY) + imageOffsetY);
+            else -- if the graphical change is not a font or image change then dont alter anything
                 tillNextSpace = string.sub(tillNextSpace, 0, ind - 1) .. string.sub(tillNextSpace, ind + 1,-1); -- remove the graphical change from the text
+            end
+
+            -- these elseifs are for setting local variables for easier scoping
+            if string.find(modif, "^imageScale=.*$") then
+                imageScale = tonumber(string.match(modif, "[^=]*$"));
+            elseif string.find(modif, "^imageScaleX=.*$") then
+                imageScaleX = tonumber(string.match(modif, "[^=]*$"));
+            elseif string.find(modif, "^imageScaleY=.*$") then
+                imageScaleY = tonumber(string.match(modif, "[^=]*$"));
+            elseif string.find(modif, "^imageOffsetX=.*$") then
+                imageOffsetX = tonumber(string.match(modif, "[^=]*$"));
+            elseif string.find(modif, "^imageOffsetY=.*$") then
+                imageOffsetY = tonumber(string.match(modif, "[^=]*$"));
+            elseif string.find(modif, "^imageBuffer=.*$") then
+                imageBuffer = tonumber(string.match(modif, "[^=]*$"));
+            elseif string.find(modif, "^imageWidth=.*$") then
+                imageWidth = tonumber(string.match(modif, "[^=]*$"));
+            elseif string.find(modif, "^imageHeight=.*$") then
+                imageHeight = tonumber(string.match(modif, "[^=]*$"));
             end
 
             table.insert(tempAddLine, {charStart = ind, modificationName = string.match(modif, "^[^=]*"), setTo = string.match(modif, "[^=]*$")});
@@ -271,8 +312,22 @@ function FancyText:getWidth()
 end
 
 function FancyText:draw()
-    local colour = self.DEFAULT_COLOURS.white;
+    local colour = self.DEFAULT_COLOURS.black;
     local highlight = self.DEFAULT_COLOURS.clear;
+
+    local otherValues = {
+        imageScale = 1; -- scale x and y
+        imageScaleX = 1; -- scale x
+        imageScaleY = 1; -- scale y
+        imageOffsetX = 0; -- move to the right by pixels
+        imageOffsetY = 0; -- move down by pixels
+        imageBuffer = 0; -- move following items right by pixels
+        imageWidth = nil; -- force the image to be this wide
+        imageHeight = nil; -- force the image to be this tall
+        imageRotation = 0; -- rotate by radians
+        imageRotateX = 0.5; -- perun of where to rotate around
+        imageRotateY = 0.5; -- perun of where to rotate around
+    };
 
     local scale = self.textHeight / self.fontHeight;
 
@@ -330,6 +385,33 @@ function FancyText:draw()
                     end
 
                     love.graphics.setFont(curFont);
+                elseif w.modificationName == "image" then
+                    love.graphics.setColor(1,1,1,1); -- white; temporarily
+
+                    local img = self.pointer[w.setTo];
+                    local newWidth  = otherValues.imageWidth  or (img:getWidth()  * otherValues.imageScale * otherValues.imageScaleX);
+                    local newHeight = otherValues.imageHeight or (img:getHeight() * otherValues.imageScale * otherValues.imageScaleY);
+
+                    love.graphics.draw(
+                        img,
+                        x + otherValues.imageOffsetX + otherValues.imageRotateX * newWidth,
+                        y + otherValues.imageOffsetY + otherValues.imageRotateY * newHeight + v.height - newHeight,
+                        otherValues.imageRotation,
+                        newWidth / img:getWidth(),
+                        newHeight / img:getHeight(),
+                        img:getWidth()  * otherValues.imageRotateX,
+                        img:getHeight() * otherValues.imageRotateY
+                    );
+
+                    x = x + newWidth + otherValues.imageOffsetX + otherValues.imageBuffer;
+                else
+                    local set = tonumber(w.setTo);
+
+                    if not set then
+                        set = self.pointer[w.setTo];
+                    end
+
+                    otherValues[w.modificationName] = set;
                 end
             end
 
