fd0831b3548840df322bdc8ef26e783cdff95002
skagonGamer <judah.cline@outlook.com>
2025-11-02 11:55:37 -0500

    pain

--- tutorialHelper/init.lua
+++ tutorialHelper/init.lua
@@ -16,7 +16,47 @@ function TutorialHelper.new()
     instance.fancyTextPointer = {}; -- not intel in order to prevent fancText from constantly updating when not needed
     instance.fancyText = FancyText.new("", 0, 0, 200, 24, "left", nil, instance.fancyTextPointer);
 
+    instance.animations = {}; -- paired table of animations in the format of:
+    -- { -- animation table
+    --      pair = { -- animation
+    --          [1] = texture; -- animations frame 1
+    --          [2] = texture; -- animations frame 2 .etc
+    --          framerate = 60; -- how many times does the frame change per second
+    --          transition = pair; -- what animation to transition into (loop if nil)
+    --      }
+    -- }
+    instance.animation = nil; -- pointer to the currently playing animation
+    instance.animationFrame = 1; -- index of the current animation frame
+    instance.animationTime = 0; -- amount of seconds the animation has been playing for
+
+    instance.x = 0; -- position of the character
+    instance.y = 0;
+
+    instance.visible = true; -- if the character is visible
+
     return instance;
+end
+
+function TutorialHelper:addAnimation(name, frames, framerate, transition)
+    if transition and not self.animations[transition] then
+        print("tried to transition to animation that does not exist yet: " .. transition);
+    end
+
+    frames.framerate  = framerate  or 60;
+    frames.transition = transition or nil; -- for clarity
+    self.animations[name] = frames;
+end
+
+function TutorialHelper:setVisibility(visible)
+    self.visible = visible;
+end
+
+function TutorialHelper:setAnimation(animation)
+    assert(self.animations[animation], "tried to set animation to animation that does not exist: " .. animation);
+
+    self.animation = self.animations[animation];
+    self.animationFrame = 1; -- restart animation
+    self.animationTime = 0; -- restart animation
 end
 
 function TutorialHelper:addConnection(from, to, ...)
@@ -24,6 +64,7 @@ function TutorialHelper:addConnection(from, to, ...)
 
     assert(self.tree[from], "tried to create a connection from a node that does not exist: " .. from);
     assert(self.tree[to], "tried to create a connection to a node that does not exist: " .. to);
+    assert(self.tree[from].connections[to] == nil, "tried to add a connection that already existed: ".. from .. " to " .. to);
 
     self.tree[from].connections[to] = args;
 end
@@ -51,7 +92,29 @@ function TutorialHelper:setNode(name)
     self.fancyText:setText("");
 end
 
+function TutorialHelper:setPosition(x, y)
+    self.x = x;
+    self.y = y;
+
+    --! magic numbers
+    self.fancyText:setPosition(x - 240, y - 50);
+end
+
 function TutorialHelper:update(dt)
+    -- update animation
+    if self.animation then
+        self.animationTime = self.animationTime + dt;
+
+        self.animationFrame = math.floor(self.animationTime / self.animation.framerate);
+
+        if self.animation.transition and self.animationFrame >= #self.animation then
+            self:setAnimation(self.animation.transition);
+        end
+
+        self.animationFrame = self.animationFrame % #self.animation + 1;
+    end
+
+    -- update speech bubble
     if self.speaking then
         self.speechTimer = self.speechTimer + dt;
 
@@ -68,6 +131,7 @@ function TutorialHelper:update(dt)
         return;
     end
 
+    -- try to find next text to say
     for connect, connection in pairs(self.tree[self.currentNode].connections) do
         local connects = true;
 
@@ -130,6 +194,13 @@ function TutorialHelper:sendInfo(name, value)
 end
 
 function TutorialHelper:draw()
+    love.graphics.setColor(1,1,1); -- white
+
+    if self.animation then
+        love.graphics.draw(self.animation[self.animationFrame], self.x, self.y);
+    end
+
+    self.fancyText:draw();
 end
 
 return TutorialHelper;
