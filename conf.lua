570d0a16e24704181f263bcb740b01339dca8745
moarzz <xaviermarseille@hotmail.ca>
2026-01-07 00:44:45 -0500

    I forgor this

--- conf.lua
+++ conf.lua
@@ -1,20 +1,19 @@
 function love.conf(t)
-    t.identity = 'brickbreaker-lua'
-    t.version = "11.4"
+    t.identity = 'brickbreaker-lua'           -- The name of the save directory
+    t.version = "11.4"                        -- The LÖVE version this game was made for
 
     t.console = true;
 
     -- Window settings
-    t.window.title = "Brick Breaker"
-    t.window.width = 960
-    t.window.height = 540
-    t.window.fullscreen = false
-    t.window.vsync = 0
+    t.window.title = "Brick Breaker"          -- The window title
+    t.window.width = 960                     -- The window width
+    t.window.height = 540                    -- The window height
+    t.window.fullscreen = false                -- Enable fullscreen
+    t.window.vsync = 0                        -- Disable VSync
 
-    -- ADD THESE LINES HERE:
-    t.window.resizable = true
-    t.window.highdpi = false     -- Browsers struggle with HighDPI + complex shaders
-    t.window.usedpiscale = false -- Keep coordinates consistent for shaders
-    t.window.stencil = true      -- Some shaders need this for masking
-    t.window.depth = 32          -- This forces RGBA8 instead of RGBA4
+    t.window.resizable = true;
+
+    -- Performance
+    --t.gammacorrect = true                    -- Enable gamma-correct rendering
+    --t.window.refreshrate = 120                -- Target refresh rate (cap FPS)
 end
