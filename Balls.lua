189064c796c27761db72a1e2fc9558cd097ede6d
moarzz <xaviermarseille@hotmail.ca>
2026-01-14 03:12:53 -0500

    10% gun buffs

--- Balls.lua
+++ Balls.lua
@@ -1305,13 +1305,13 @@ local function shoot(gunName, ball)
             if gun.currentAmmo > 0 then
                 if gun.name == "Minigun" then
                     local sprayMult = hasItem("Four Leafed Clover") and 0.56 or 0.714
-                    local timeUntilNextShot = gun.fireRateMult * 1.1 * (mapRangeClamped(getStat("Minigun", "ammo") - gun.currentAmmo, 0, 25, 4, 0.5) * (spray and sprayMult or 1))/(getStat(gun.name, "fireRate") * bulletStormMult)
+                    local timeUntilNextShot = gun.fireRateMult * 1 * (mapRangeClamped(getStat("Minigun", "ammo") - gun.currentAmmo, 0, 25, 4, 0.5) * (spray and sprayMult or 1))/(getStat(gun.name, "fireRate") * bulletStormMult)
                     Timer.after(timeUntilNextShot, function() shoot(gunName) end)
                     createFireRateVFX(timeUntilNextShot)
                     -- createCooldownVFX(cooldownValue)
                 else
                     local sprayMult = hasItem("Four Leafed Clover") and 0.56 or 0.714
-                    local timeUntilNextShot = (gun.fireRateMult * 3.0 * 1.1 * (spray and sprayMult or 1))/(getStat(gun.name, "fireRate") * bulletStormMult)
+                    local timeUntilNextShot = (gun.fireRateMult * 3.0 * 1 * (spray and sprayMult or 1))/(getStat(gun.name, "fireRate") * bulletStormMult)
                     Timer.after(timeUntilNextShot, function() shoot(gunName) end)
                     createFireRateVFX(timeUntilNextShot)
                     -- createCooldownVFX(cooldownValue)
